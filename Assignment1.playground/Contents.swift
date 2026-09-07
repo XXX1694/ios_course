2fd9a2c5c1d29c930ca1f5ccd32b207e67b1b2c3
SERIKABZ <abzal.serikbay@bcchub.kz>
2026-09-07 11:21:07 +0500

    step2: add hobbies

--- Assignment1.playground/Contents.swift
+++ Assignment1.playground/Contents.swift
@@ -5,3 +5,10 @@ let birthYear = 2004
 let age = currentYear - birthYear
 let isStudent = true
 let height = 1.88
+let isTall = height > 1.8 // I forgot to add some info "Add some more details about you"
+
+let hobby = "Developing mobile apps"
+let numberOfHobbies = 2
+let favoriteNumber = 100000000000
+let isHobbyCreative = true
+let whenIStatredCoding = 2015 // From 5th grade )))

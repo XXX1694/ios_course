41ef1b06976af645db9c7f2e8fbfeab10eaf8284
SERIKABZ <abzal.serikbay@bcchub.kz>
2026-09-07 11:14:41 +0500

    step1: create variables

--- Assignment1.playground/Contents.swift
+++ Assignment1.playground/Contents.swift
@@ -1,1 +1,7 @@
-
+let firstName = "Abzal"
+let lastName = "Serikbay"
+let currentYear = 2026 // Bonus Challenge
+let birthYear = 2004
+let age = currentYear - birthYear
+let isStudent = true
+let height = 1.88

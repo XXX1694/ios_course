e997e9b25376955c5f1f422c03895bea30e4cd10
Abzal Serikbay <serikbay.a04@gmail.com>
2026-09-12 11:32:56 +0500

    ass2: medium tasks

--- Assignment2.playground/Contents.swift
+++ Assignment2.playground/Contents.swift
@@ -19,3 +19,23 @@ print(progLang["Swift"] ?? 2014)
 var colors: [String] = ["red", "green", "blue", "yellow"]
 colors[1] = "purple"
 print(colors)
+
+
+// Medium tasks
+
+var set1: Set<Int>  = [1, 2, 3, 4];
+var set2: Set<Int>  = [3, 4, 5, 6];
+print(set1.intersection(set2))
+
+var studetGrades: [String: Int] = [
+    "Abzal": 90,
+    "Arman": 98,
+    "Borat": 69,
+]
+studetGrades["Borat"] = 70
+print(studetGrades)
+
+var fruits1: [String] = ["apple", "banana"]
+var fruits2: [String] = ["cherry", "date"]
+var allFruits: [String] = fruits1 + fruits2
+print(allFruits)
